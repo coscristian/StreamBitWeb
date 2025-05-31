1915aafadba54a1afabd2ba59fb600207a85ddeb
Cristian Quesada Cossio <cristian.quesada@utp.edu.co>
2025-05-31 02:49:57 -0500

    (feat): Added styles to navbar menu

--- src/app/core/home/components/home.component.ts
+++ src/app/core/home/components/home.component.ts
@@ -1,11 +1,66 @@
-import { Component, Injectable } from "@angular/core";
-
+import {Component, OnInit} from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { Menubar } from 'primeng/menubar';
+import {MainNavbarComponent} from '../../../shared/components/streambit-main-navbar/streambit-main-navbar.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [BadgeModule, AvatarModule, InputTextModule, CommonModule, MainNavbarComponent]
 })
-export class HomeComponent {
-  constructor() { }
+export class HomeComponent implements OnInit {
+  items: MenuItem[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Content',
+        icon: 'pi pi-star',
+      },
+      {
+        label: 'Scores',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Components',
+            icon: 'pi pi-bolt',
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server',
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+              },
+            ],
+          },
+        ],
+      }
+    ];
+  }
 }
